8cd45295a37987de43e3fad8658123f140fb997c
Ibra Al Tabian <92589376+ibraaltabian17@users.noreply.github.com>
2025-11-25 21:52:50 +0700

    cloper

--- bulid.sh
+++ bulid.sh
@@ -1,6 +1,6 @@
 rm -rf .repo/local_manifests/ 
-repo init -u https://github.com/halcyonproject/manifest.git -b 16.0 --git-lfs 
- git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b halcyon-16 .repo/local_manifests &&  
+repo init -u https://github.com/The-Clover-Project/manifest.git -b 16-qpr1 --git-lfs 
+git clone https://github.com/ibraaltabian17/local_manifest.git --depth 1 -b clover-16 .repo/local_manifests &&  
 # Sync the repositories
 if [ -f /usr/bin/resync ]
  then
@@ -10,12 +10,10 @@ else
 fi && 
 # Set up build environment
 export BUILD_USERNAME=ibratabian17 
- export BUILD_HOSTNAME=crave 
+export BUILD_HOSTNAME=prjktla 
  source build/envsetup.sh
 
-# Hax
-cd packages/modules/Connectivity && git fetch https://github.com/LineageOS/android_packages_modules_Connectivity refs/changes/72/452772/1 && git cherry-pick FETCH_HEAD
 cd ../../../hardware/lineage/compat && git fetch https://github.com/yaap/hardware_lineage_compat && git checkout FETCH_HEAD && cd ../../../
 
 # Build the ROM
-lunch halcyon_beryllium-bp2a-userdebug && mka carthage
+lunch clover_beryllium-bp3a-userdebug && mka clover
