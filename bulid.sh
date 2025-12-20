b4bfa718f85fbdda9b77406f4bef6b0ca884abcd
Ibra Al Tabian <92589376+ibraaltabian17@users.noreply.github.com>
2025-12-20 14:07:33 +0700

    halcyon

--- bulid.sh
+++ bulid.sh
@@ -13,9 +13,5 @@ export BUILD_USERNAME=ibratabian17
  export BUILD_HOSTNAME=crave 
  source build/envsetup.sh
 
-# Hax
-cd packages/modules/Connectivity && git fetch https://github.com/LineageOS/android_packages_modules_Connectivity refs/changes/72/452772/1 && git cherry-pick FETCH_HEAD
-cd ../../../hardware/lineage/compat && git fetch https://github.com/yaap/hardware_lineage_compat && git checkout FETCH_HEAD && cd ../../../
-
 # Build the ROM
 lunch halcyon_beryllium-bp2a-userdebug && mka carthage
